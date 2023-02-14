f85c6fe7cbdea47599f759a709e3c8543b8cc3b9
Spencer Belton <sbelton@Spencers-MacBook-Air.local>
2023-02-13 17:53:23 -0700

    initial commit, setting project up

---
+++ LeSchnoz/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LeSchnoz
+//
+//  Created by Spencer Belton on 2/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LeSchnoz/LeSchnozApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeSchnozApp.swift
+//  LeSchnoz
+//
+//  Created by Spencer Belton on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LeSchnozApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
